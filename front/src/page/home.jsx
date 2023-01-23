9916c14ff159678e9d48cc2525678b9dc6ad6c92
shany <shany.razafindramary@epitech.eu>
2023-01-23 17:26:29 +0100

    [ADD] page => home ; component => navbar ; dependancy

---
+++ front/src/page/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import axios from 'axios';
+import Navbar from '../component/navbar';
+
+const Home = () => {
+
+    return(
+        <section className='home flex'>
+            <Navbar/>
+            coucou
+        </section>
+    )
+}
+
+export default Home
