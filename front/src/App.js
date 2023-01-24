44577eb742b490c0ac743cbc082e1b9d86fef646
shany <shany.razafindramary@epitech.eu>
2023-01-24 17:33:41 +0100

    [ADD] .env

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Page from "./page/page";
+import Login from './page/auth';
+import Connexion from './page/connexion';
 
 import "./css/style.css";
 import "./css/home.css";
@@ -10,6 +12,8 @@ function App() {
     <Router>
       <Routes>
         <Route exact path='/' element={<Page />}></Route>
+        <Route exact path='/auth' element={<Login />}></Route>
+        <Route path='/connexion' element={<Connexion/>}></Route>
       </Routes>
     </Router>
   );
