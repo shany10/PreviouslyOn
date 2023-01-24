0561d44648f8160b574f4b56d9ca7d2195e95128
shany <shany.razafindramary@epitech.eu>
2023-01-24 12:15:49 +0100

    [UPDATE] app , [ADD] page => research , serie , movie , category

--- front/src/component/navbar.jsx
+++ front/src/component/navbar.jsx
@@ -4,16 +4,16 @@ import fleche_droite from '../img/icons8-flèche-droite-60.png';
 import photo_profil from '../img/photo-avatar-profil.png';
 import logo from '../img/icons8-fox-64.png';
 
-const Navbar = () => {
+const Navbar = ({ changePage }) => {
 
     const [isOpen, setIsOpen] = useState(false)
 
     const [bgc, setBgc] = useState({
-        recherche: 'transparent',
-        acceuil: 'rgb(73, 73, 73)',
-        serie: 'transparent',
-        film: 'transparent',
-        categorie: 'transparent'
+        Research: 'transparent',
+        Home: 'red',
+        Serie: 'transparent',
+        Movie: 'transparent',
+        Category: 'transparent'
     })
 
     const variants = {
@@ -24,16 +24,13 @@ const Navbar = () => {
     const current_page = (e) => {
         setBgc({
             ...bgc,
-            recherche:'transparent',
-            acceuil: 'transparent',
-            serie: 'transparent',
-            film:'transparent',
-            categorie:'transparent',
-            [e.target.id]: 'rgb(73, 73, 73)'
+            Research: 'transparent',
+            Home: 'transparent',
+            Serie: 'transparent',
+            Movie: 'transparent',
+            Category: 'transparent',
+            [e.target.id]: 'red'
         });
-
-        // setBgc({ ...bgc, [e.target.id]: 'rgb(73, 73, 73)' })
-        console.log(bgc)
     }
 
     return (
@@ -43,11 +40,36 @@ const Navbar = () => {
                     <img src={photo_profil} alt="profil" className='nav-photo-profil' />
                 </div>
                 <div className='nav-fleche txt-center'>
-                    <motion.p whileHover={{ scale: 1.2 }} onClick={(e) => current_page(e)} style={{ backgroundColor: bgc.recherche }} id='recherche' className='fts-2 nav-item pointer'>Recherche</motion.p>
-                    <motion.p whileHover={{ scale: 1.2 }} onClick={(e) => current_page(e)} style={{ backgroundColor: bgc.acceuil }} id='acceuil' className='fts-2 nav-item pointer'>Acceuil</motion.p>
-                    <motion.p whileHover={{ scale: 1.2 }} onClick={(e) => current_page(e)} style={{ backgroundColor: bgc.serie }} id='serie' className='fts-2 nav-item pointer'>Serie</motion.p>
-                    <motion.p whileHover={{ scale: 1.2 }} onClick={(e) => current_page(e)} style={{ backgroundColor: bgc.film }} id='film' className='fts-2 nav-item pointer'>Film</motion.p>
-                    <motion.p whileHover={{ scale: 1.2 }} onClick={(e) => current_page(e)} style={{ backgroundColor: bgc.categorie }} id='categorie' className='fts-2 nav-item pointer'>Categorie</motion.p>
+                    <motion.p whileHover={{ scale: 1.2 }}
+                        onClick={(e) => {current_page(e) ; changePage(e.target.id)}}
+                        style={{ backgroundColor: bgc.Research }}
+                        id='Research' className='fts-2 nav-item pointer'>
+                        Recherche
+                    </motion.p>
+                    <motion.p whileHover={{ scale: 1.2 }}
+                        onClick={(e) => {current_page(e); changePage(e.target.id)}}
+                        style={{ backgroundColor: bgc.Home }}
+                        id='Home' className='fts-2 nav-item pointer'>
+                        Acceuil
+                    </motion.p>
+                    <motion.p whileHover={{ scale: 1.2 }}
+                        onClick={(e) => {current_page(e); changePage(e.target.id)}}
+                        style={{ backgroundColor: bgc.Serie }}
+                        id='Serie' className='fts-2 nav-item pointer'>
+                        Serie
+                    </motion.p>
+                    <motion.p whileHover={{ scale: 1.2 }}
+                        onClick={(e) => {current_page(e); changePage(e.target.id)}}
+                        style={{ backgroundColor: bgc.Movie }}
+                        id='Movie' className='fts-2 nav-item pointer'>
+                        Film
+                    </motion.p>
+                    <motion.p whileHover={{ scale: 1.2 }}
+                        onClick={(e) => {current_page(e); changePage(e.target.id)}}
+                        style={{ backgroundColor: bgc.Category }}
+                        id='Category' className='fts-2 nav-item pointer'>
+                        Categorie
+                    </motion.p>
                     <div className='flex center'>
                         <img src={logo} alt="logo" />
                     </div>
